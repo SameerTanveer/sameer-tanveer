12ed0171d0f63b8e2220ac55ef0d305500c3e059
Sameer Tanveer <sameertanveer@iourology.com>
2025-06-20 14:16:32 +0500

    added new project and updated js to remove jquery

--- js/resume.js
+++ js/resume.js
@@ -1,28 +1,47 @@
-(function($) {
+(function () {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+  // Smooth scrolling using native JS
+  document
+    .querySelectorAll('a.js-scroll-trigger[href*="#"]:not([href="#"])')
+    .forEach(function (link) {
+      link.addEventListener("click", function (e) {
+        const pathname = window.location.pathname.replace(/^\//, "");
+        const linkPath = link.pathname.replace(/^\//, "");
+        if (
+          pathname === linkPath &&
+          window.location.hostname === link.hostname
+        ) {
+          let target =
+            document.querySelector(link.hash) ||
+            document.getElementsByName(link.hash.slice(1))[0];
+          if (target) {
+            e.preventDefault();
+            window.scrollTo({
+              top: target.getBoundingClientRect().top + window.scrollY,
+              behavior: "smooth",
+            });
+          }
+        }
+      });
+    });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  document.querySelectorAll(".js-scroll-trigger").forEach(function (el) {
+    el.addEventListener("click", function () {
+      const navCollapse = document.querySelector(".navbar-collapse");
+      if (navCollapse && navCollapse.classList.contains("show")) {
+        // Bootstrap 5 collapse
+        const bsCollapse = bootstrap.Collapse.getOrCreateInstance(navCollapse);
+        bsCollapse.hide();
+      }
+    });
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
-
-})(jQuery); // End of use strict
+  // Activate scrollspy (Bootstrap 5)
+  if (window.bootstrap && document.body) {
+    bootstrap.ScrollSpy.getOrCreateInstance(document.body, {
+      target: "#sideNav",
+    });
+  }
+})(); // End of use strict
